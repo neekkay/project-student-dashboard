39613c0f25246a97411b5ff76e4c06dee20a9456
USERNAME <EMAIL>
2023-08-25 17:27:39 -0400

    semester list is connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,46 @@
- import React,{useState} from "react";
- import studentsData from "./data/data.json";
- import StudentList from "./Component/StudentList";
- import StudentCard from "./Component/StudentCard";
+import React, { useState } from "react";
+import studentsData from "./data/data.json";
+import StudentList from "./Component/StudentList";
+import StudentCard from "./Component/StudentCard";
 import SemesterList from "./Component/SemesterList";
- import "./index.css";
+import "./index.css";
+
 function App() {
-
-const [filteredStudent, setFilteredStudent] = useState([...studentsData]);
-
   const [selectedSemester, setSelectedSemester] = useState(null);
 
-const [SelectedCohortCode, setSelectedCohortCode] = useState(null);
-
-  const handleSemesterClick = (semester, cohortCode) =>{
+  const handleSemesterClick = (semester) => {
     setSelectedSemester(semester);
-    setSelectedCohortCode(cohortCode);
-  
-  // console.log(studentsData);
-  
   };
 
-const filteredStudents = studentsData.filter((student) => {
-  if (selectedSemester && SelectedCohortCode) {
-    return (
-      student.cohort.cohortCode === selectedSemester && student.cohort.cohortCode === setSelectedCohortCode
-    );
-}else if (selectedSemester) {
-  return student.cohort.cohortCode === selectedSemester;
-} else{
-  return true;
-
-}
-});
-
+  const filteredStudents = studentsData.filter((student) => {
+    if (selectedSemester) {
+      const formattedSelectedSemester = selectedSemester.replace(/\s/g, '');
+      
+      return  formattedSelectedSemester === student.cohort.cohortCode ;
+    } else {
+      return true;
+    }
+  });
 
   return (
     <div className="app-container">
       <header>
-    <h1>Student Dashboard</h1>
+        <h1>Student Dashboard</h1>
       </header>
-      <aside className="semester-list">
-        <h2>Choose a Class by Start Date</h2>
-        <ul>
-          <li className="clickable" onClick={() => handleSemesterClick('Winter 2026')}>Winter 2026 </li>
-          <li className="clickable" onClick={() => handleSemesterClick('Fall2026')}>Fall 2026 </li>
-         <li className="clickable" onClick={() => handleSemesterClick('Summer2026')}>Summer 2026 </li>
-         <li className="clickable" onClick={() => handleSemesterClick('Spring2026')}>Spring 2026 </li>
-         <li className="clickable" onClick={() => handleSemesterClick('Winter2025')}>Winter 2025 </li>
-         <li className="clickable" onClick={() => handleSemesterClick('Fall2025')}>Fall 2025 </li>
-         <li className="clickable" onClick={() => handleSemesterClick('Summer2025')}>Summer 2025 </li>
-         <li className="clickable" onClick={() => handleSemesterClick('Spring2025')}>Spring 2025 </li>
-         <li className="clickable" onClick={() => handleSemesterClick('Winter2024')}>Winter 2024 </li>
-         <li className="clickable" onClick={() => handleSemesterClick('Fall2024')}>Fall 2024 </li>
-         <li className="clickable" onClick={() => handleSemesterClick('Summer2024')}>Summer 2024 </li>
-         <li className="clickable" onClick={() => handleSemesterClick('Spring2024')}>Spring 2024 </li>
-         <li className="clickable" onClick={() => handleSemesterClick('Winter2023')}>Winter 2023 </li>
-         <li className="clickable" onClick={() => handleSemesterClick('Fall2023')}>Fall 2023 </li>
-         <li className="clickable" onClick={() => handleSemesterClick('Summer2023')}>Summer 2023 </li>
-         <li className="clickable" onClick={() => handleSemesterClick('Spring2023')}>Spring 2023 </li>
-        </ul>
+      <aside>
+        <SemesterList
+          handleSemesterClick={handleSemesterClick}
+          selectedSemester={selectedSemester}
+        />
       </aside>
       <div className="student-list">
-      <h2>All Students:</h2>
-      {filteredStudent.map((student) => {
-        return <StudentList  key={student.id} student={student} />;
-      })}
-      <div className="student-card">
-      <h2>Students Card</h2>
-      {filteredStudent.map((student) => {
-        return <StudentCard key={student.id} student={student} className="StudentCard" />;
-        
-      })}
-      console.log(StudentCard);
+        <h2>All Students:</h2>
+        {filteredStudents.map((student) => (
+          <StudentList key={student.id} student={student} />
+        ))}
       </div>
-    </div>
-    </div>
-  
-  
+       </div>
   );
-    }
+}
+
 export default App;
--- src/Component/SemesterList.jsx
+++ src/Component/SemesterList.jsx
@@ -1,33 +1,41 @@
 import React from "react";
-  
- function SemesterList ({handleSemesterClick, selectedSemester}) {
+
+function SemesterList({ handleSemesterClick, selectedSemester }) {
+	
 	const semesters = [
-		"Winter2026",
-		"Fall2026",
-		"Summer2026",
-		"Spring2026",
-		"Winter2025",
-		"Fall2025",
-		"Summer2025",
-		"Spring2025",
-		"Winter2024",
-		"Fall2024",
-		"Summer2024",
-		"Spring2024",
-		"Winter2023",
-		"Fall2023",
-		"Summer2023",
-		"Spring2023"	
+		"Winter 2026",
+		"Fall 2026",
+		"Summer 2026",
+		"Spring 2026",
+		"Winter 2025",
+		"Fall 2025",
+		"Summer 2025",
+		"Spring 2025",
+		"Winter 2024",
+		"Fall 2024",
+		"Summer 2024",
+		"Spring 2024",
+		"Winter 2023",
+		"Fall 2023",
+		"Summer 2023",
+		"Spring 2023"
 	];
 
 	return (
 		<aside className="semester-list">
 			<h2>Choose a class by Start Date</h2>
 			<ul>
-				<li key = {semester} onClick={() => handleSemesterClick(semester)}>{semester}
-			</li>
+				{semesters.map(semester => (
+					<li
+					 key={semester}
+					 className={selectedSemester === semester ? "clickable selected" : "clickable"}
+					 onClick={() => handleSemesterClick(semester)}>
+						{semester}
+					</li>
+				))}
 			</ul>
 		</aside>
-	)
+	);
 }
+
 export default SemesterList;
